2cefab4c090f10f6381ba74c008adaadcf3b28ae
Marek Gierlicki <mgierlicki@anteeo.com.pl>
2025-01-18 12:20:40 +0100

    Auth UI

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
+
+const Page = () => {
+  return (
+    <AuthForm
+      type="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={() => {}}
+    />
+  );
+};
+
+export default Page;
