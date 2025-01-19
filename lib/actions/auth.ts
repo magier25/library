517b992a82b436bd6b86d39d1a5e7ad0bbbebb87
Marek Gierlicki <mgierlicki@anteeo.com.pl>
2025-01-19 19:09:05 +0100

    Resend without real sending emails

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -6,8 +6,10 @@ import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-import ratelimit from "../ratelimit";
+import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "@/lib/workflows";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -68,6 +70,14 @@ export const signUp = async (params: AuthCredentials) => {
       password: hashedPassword,
       universityId,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     await signInWithCredentials({ email, password });
--- lib/workflows.ts
+++ lib/workflows.ts
@@ -1,7 +1,39 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "./config";
 
-export const workfloowClient = new WorkflowClient({
+export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  console.log("Sending email to:", email);
+  console.log("Subject", subject);
+  console.log("Message", message);
+
+  /*  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "fHnXb@example.com",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });*/
+};
